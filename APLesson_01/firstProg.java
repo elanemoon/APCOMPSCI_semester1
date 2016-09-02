a6386fc86ca495763cc24dc9c017913afd1d5385
elanemoon <MoonS7054@sduhsd.lan>
2016-09-02 13:39:54 -0700

    changed firstprog

--- APLesson_01/firstProg.java
+++ APLesson_01/firstProg.java
@@ -2,8 +2,6 @@ public class firstProg
 {
      public static void main(String[]args)
 	 {
-       System.out.println("^___^");
-	   System.out.println("(..) ");
-	   System.out.println("\\   \\/  / ");
+       System.out.println("hello world");
 	 }
 }
