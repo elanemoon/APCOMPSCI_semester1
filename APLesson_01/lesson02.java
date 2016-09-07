ff8707c47a0a47f960251bc7f5ccb3504d64c2f0
elanemoon <MoonS7054@sduhsd.lan>
2016-09-07 13:58:03 -0700

    Added Ap Lesson 2

---
+++ APLesson_01/lesson02.java
@@ -0,0 +1,8 @@
+public class lesson02
+{
+       public static void main(String[]args)
+	   {
+	       String name = "I like brownies";
+		   System.out.println(name);
+	   }
+}
---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,21 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int recArea=l*w;
+		 
+	    System.out.println("The area of your rectangle is " + recArea);
+		String name = "Elane Moon";
+		String address = " 6485 Cypress Meadows Trail";
+		String city = " San Diego";
+		String zipcode = " 92130";
+		System.out.println(name +  address +  city +  zipcode);
+		int a = 5;
+		int b = 4;
+		int c = 5;
+		int sa= 2*a*b + 2*a*c + 2*b*c;
+		System.out.println("The surface area of your rectangle is " + sa);
+	}
+}
