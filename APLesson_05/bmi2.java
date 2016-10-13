59ea2e759330c65c1a758cc8636994d15e875f3c
elanemoon <eggtotheplant@gmail.com>
2016-10-13 14:34:51 -0700

    Added AP Lesson_05

--- APLesson_05/bmi2.java
+++ APLesson_05/bmi2.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 public class bmi2
 {
+	static double h;
+	static double w;
+	static double bmi;
+	static String condition;
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter your height: ");
-		double height = kb.nextDouble();
+		h = kb.nextDouble();
 		System.out.println("Please enter your weight: ");
-		double weight = kb.nextDouble();
-		
-		double bmi = weight / height*height;
-		System.out.println("Your BMI is" + bmi);
-		String condition = "  ";
-	
+		w = kb.nextDouble();
+	    calcCondish();
+		System.out.println("Your BMI is " + bmi);
+	   System.out.println("You are " + condition);
+	}
+	public static void calcCondish()
+	{
+	     bmi = w / h*h;
 		if(39.9 < bmi)
 			condition = "Morbidly Obese";
 		else if(35 < bmi)
 			condition = "Very Obese";
-		else if(34.9 >= bmi >= 29.9)
+		else if(34.9 > bmi)
 			condition = "Obese";
-		else if(29.9 >= bmi >= 25)
+		else if(29.9 > bmi)
 			condition = "Overweight";
-		else if(24.9 >= bmi >= 18.5)
+		else if(24.9 > bmi)
 			condition = "Normal";
-		else if(18.5 >= bmi)
+		else if(18.5 > bmi)
 			condition = "Underweight";
-	   System.out.println("You are" + condition);
+	}	   
 }
 
-}
