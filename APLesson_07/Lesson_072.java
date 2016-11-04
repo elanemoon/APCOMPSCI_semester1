65fc0edb9d9fa51b8be28a883c5416eb81e02355
elanemoon <eggtotheplant@gmail.com>
2016-11-04 14:18:43 -0700

    Added APLesson_07

---
+++ APLesson_07/Lesson_072.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Lesson_072
+{
+	static String sentence;
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a sentence: ");
+		sentence = kb.nextLine();
+
+			while(sentence.indexOf(" ") >= 0)
+			{
+				sentence = sentence.substring(0, sentence.indexOf(" ")) + sentence.substring(sentence.indexOf(" ")+1);
+			}
+		System.out.println("Wihtout spaces...." + sentence);
+	}
+}
