15c84141d8e0ff3823a281aad1fe32eac3ddbad0
elanemoon <eggtotheplant@gmail.com>
2016-11-15 14:32:39 -0800

    Added Lesson_09

---
+++ APLesson_09/averagenumbers.java
@@ -0,0 +1,27 @@
+public class averagenumbers
+{
+	public static void main(String[]args)
+	{  
+		int[] numbers = new int[10];
+		for(int i = 0; i < numbers.length; i++)
+		{
+			numbers[i] = (int)(Math.random()*100) +1;
+		}
+		System.out.println("Numbers...");
+		for(int number : numbers)
+		{
+			System.out.print(number + " ");
+		}
+		System.out.println();
+		System.out.println( "The average of the above numbers is... " + average(numbers));
+	}
+	public static void average(int [] numbers)
+	{
+		int average = 0;
+		for(int number : numbers)
+		{
+		 return	number + sum; 
+		}
+		return average/ 10;
+	}
+}
---
+++ APLesson_09/reverseword.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class reverseword
+{
+	public static void main(String[]args)
+	{
+		
+		String[] names = new String[5];
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter 5 names: ");
+	
+		for(int i = 0; i < names.length; i++)
+		{
+			names[i] = kb.next();
+		}
+		System.out.println("in order..." );
+		for(String name : names)
+		{
+			System.out.println(name);
+		}
+		System.out.println();
+		System.out.println("reversed...");
+		reverse(names);
+		
+	}
+		public static void reverse(String [] n)
+		{
+		   for(int i = n.length-1; i >= 0; i--)
+		{
+			System.out.println(n[i]);
+		}
+		}
+}
