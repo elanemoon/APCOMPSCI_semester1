2a7e9b8ae2b5ab33f3875b92573ecde93e09a118
elanemoon <eggtotheplant@gmail.com>
2017-03-21 14:04:58 -0700

    Added Lesson 14.1 and 14.2

--- APLesson_14/APLesson_14.2/locate.java
+++ APLesson_14/APLesson_14.2/locate.java
@@ -1,5 +1,6 @@
 public interface locate
 {
    double[] getLoc();
+   void move(double x, double y);
    int getID();
 }
--- APLesson_14/APLesson_14.2/location.java
+++
@@ -1,5 +0,0 @@
-public interface locate
-{
-   double[] getLoc();
-   int getID();
-}
